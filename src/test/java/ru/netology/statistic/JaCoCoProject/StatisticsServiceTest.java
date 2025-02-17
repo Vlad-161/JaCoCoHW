81e93efcd239a1fe37e4dbaf99bac6ad26a32195
Vlad Polovinkin <polovinkinvlaaad@gmail.com>
2025-02-18 00:01:42 +0300

    Add Test

--- src/test/java/ru/netology/statistic/JaCoCoProject/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/JaCoCoProject/StatisticsServiceTest.java
@@ -15,4 +15,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 7, 13, 17, 5, 9, 5, 2, 19, 21, 17};
+
+        long expected = 21;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
